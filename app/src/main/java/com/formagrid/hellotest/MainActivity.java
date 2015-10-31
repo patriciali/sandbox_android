a1b4376727cf0775eba0c64e6b4dcc8323db0f4b
Patricia Li <patricia.li@gmail.com>
2015-10-30 18:25:47 -0700

    check memclass

--- app/src/main/java/com/formagrid/hellotest/MainActivity.java
+++ app/src/main/java/com/formagrid/hellotest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.formagrid.hellotest;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +11,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        int memoryClass = am.getMemoryClass();
+        Log.d("patricia", "memory available " + memoryClass);
     }
 
 }
