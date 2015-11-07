0ebd921aaeed7f7909b4918bfee736f60992c75d
Patricia Li <patricia.li@gmail.com>
2015-11-06 18:04:01 -0800

    sms/call intents

--- app/src/main/java/com/formagrid/hellotest/MainActivity.java
+++ app/src/main/java/com/formagrid/hellotest/MainActivity.java
@@ -1,40 +1,32 @@
 package com.formagrid.hellotest;
 
 import android.app.Activity;
-import android.app.ActivityManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.GregorianCalendar;
 
 public class MainActivity extends Activity {
-
-    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text_view);
+        String cellNumber = "657-225-1180";
 
-        String json = "20018-07-03T00:00:00.000Z";
-        GregorianCalendar cal = new GregorianCalendar(20015, 0, 3);
-        Date date = cal.getTime();
-        SimpleDateFormat mJsonDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-        SimpleDateFormat mCardDatetimeFormat = new SimpleDateFormat("LLL' 'd', 'yyyy', 'h':'mm' 'a");
+        if (false) {
+            Intent sendIntent = new Intent(Intent.ACTION_SENDTO);
+            sendIntent.setData(Uri.parse("sms:" + cellNumber));
+            sendIntent.putExtra("exit_on_sent", true);
+            startActivity(sendIntent);
+        }
 
-//        try {
-//            mTextView.setText(mCardDatetimeFormat.format(mJsonDateFormat.parse(json)));
-//            //mTextView.setText(mCardDatetimeFormat.format(date));
-//        } catch (ParseException e) {
-//            mTextView.setText(e.toString());
-//        }
-        mTextView.setText(mCardDatetimeFormat.format(null));
+        if (true) {
+            String uri = "tel:" + cellNumber;
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse(uri));
+            startActivity(intent);
+        }
     }
 
 }
