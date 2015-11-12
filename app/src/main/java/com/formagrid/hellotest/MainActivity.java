7dc59d5c6da688913195ea58a185c541a848d8b2
Patricia Li <patricia.li@gmail.com>
2015-11-11 17:13:02 -0800

    learning how to Canvas

--- app/src/main/java/com/formagrid/hellotest/MainActivity.java
+++ app/src/main/java/com/formagrid/hellotest/MainActivity.java
@@ -1,32 +1,19 @@
 package com.formagrid.hellotest;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    private CustomImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String cellNumber = "123-456-7890";
-
-        if (false) {
-            Intent sendIntent = new Intent(Intent.ACTION_SENDTO);
-            sendIntent.setData(Uri.parse("sms:" + cellNumber));
-            sendIntent.putExtra("exit_on_sent", true);
-            startActivity(sendIntent);
-        }
-
-        if (true) {
-            String uri = "tel:" + cellNumber;
-            Intent intent = new Intent(Intent.ACTION_DIAL);
-            intent.setData(Uri.parse(uri));
-            startActivity(intent);
-        }
+        mImageView = (CustomImageView) findViewById(R.id.image_view);
     }
 
 }
