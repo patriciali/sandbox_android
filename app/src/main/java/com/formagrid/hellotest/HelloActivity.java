15a74299c835be6b32bb09b2d6e8deaf8f8548aa
Patricia Li <patricia.li@gmail.com>
2016-04-29 14:32:47 -0700

    spinner shenanigans

--- app/src/main/java/com/formagrid/hellotest/HelloActivity.java
+++ app/src/main/java/com/formagrid/hellotest/HelloActivity.java
@@ -1,36 +1,28 @@
 package com.formagrid.hellotest;
 
 import android.app.Activity;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class HelloActivity extends Activity {
+
+    private Spinner mSpinner;
+    private SpinnerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("My notification")
-                        .setContentText("Hello World!");
-
-        // Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(this, HelloActivity.class);
-//        PendingIntent resultPendingIntent = ;
-//        mBuilder.setContentIntent(resultPendingIntent);
-
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // mId allows you to update the notification later on.
-        mNotificationManager.notify(6969, mBuilder.build());
+        mSpinner = (Spinner) findViewById(R.id.spinner);
+        mAdapter = new SpinnerAdapter(this);
+        mSpinner.setAdapter(mAdapter);
     }
 
     @Override
@@ -46,6 +38,54 @@ public class HelloActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    private static class SpinnerAdapter extends ArrayAdapter<String> {
+
+        private String[] mItems;
+
+        public SpinnerAdapter(Context context) {
+            super(context, R.layout.spinner_item);
+
+            mItems = new String[]{
+                    "one",
+                    "two",
+                    "three"
+            };
+        }
+
+        @Override
+        public int getCount() {
+            return mItems.length + 1;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Context context = parent.getContext();
+
+            boolean isSpecial = position == getCount() - 1;
+            String tag = isSpecial ? "special" : "normal";
+            if (convertView == null || !convertView.getTag().toString().equals(tag)) {
+                if (isSpecial) {
+                    convertView = LayoutInflater.from(context).inflate(R.layout.spinner_special_item, parent, false);
+                    convertView.setTag(tag);
+                } else {
+                    convertView = LayoutInflater.from(context).inflate(R.layout.spinner_item, parent, false);
+                    convertView.setTag(tag);
+                }
+            }
+
+            TextView textView = (TextView) convertView.findViewById(R.id.text_view);
+            textView.setText(isSpecial ? "patricia is the shit" : mItems[position]);
+
+            return convertView;
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            return this.getView(position, convertView, parent);
+        }
+
     }
 
 }
