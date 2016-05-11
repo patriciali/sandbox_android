8fd649922aa3483d1fb28106ba8b6bd0927f2335
Patricia Li <patricia.li@gmail.com>
2016-05-11 10:51:33 -0700

    fragment animation

--- app/src/main/java/com/formagrid/hellotest/HelloActivity.java
+++ app/src/main/java/com/formagrid/hellotest/HelloActivity.java
@@ -1,70 +1,24 @@
 package com.formagrid.hellotest;
 
 import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class HelloActivity extends Activity {
 
-    private static final String EXTRA_AUTOLAUNCH_FRAGMENT = "extra_autolaunch_fragment";
-
-    public static void start(Context context, boolean shouldAutolaunchFragment) {
-        Intent intent = new Intent(context, HelloActivity.class);
-        intent.putExtra(EXTRA_AUTOLAUNCH_FRAGMENT, shouldAutolaunchFragment);
-        context.startActivity(intent);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hello);
 
         TextView launchFragment = (TextView) findViewById(R.id.launch_fragment_text_view);
         launchFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showFragment();
+                HelloFragment.showFragment(HelloActivity.this);
             }
         });
-
-        TextView launchActivity = (TextView) findViewById(R.id.launch_activity_text_view);
-        launchActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AnotherActivity.start(HelloActivity.this);
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-
-        if (resultCode != RESULT_OK) {
-            return;
-        }
-
-        if (requestCode == AnotherActivity.FRAGMENT_REQUEST_CODE) {
-            Bundle extras = intent.getExtras();
-            if (extras != null) {
-                int intResult = extras.getInt(AnotherActivity.EXTRA_INT_EXTRA);
-                if (intResult == AnotherActivity.THREE) {
-                    showFragment();
-                } else {
-                    Log.d("patricia", "wrong intResult " + intResult);
-                }
-            } else {
-                Log.d("patricia", "extras == null");
-            }
-        } else {
-            Log.d("patricia", "wrong request code " + requestCode);
-        }
     }
 
     @Override
@@ -80,14 +34,6 @@ public class HelloActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-    }
-
-    private void showFragment() {
-        FragmentManager manager = getFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        HelloFragment fragment = new HelloFragment();
-        transaction.add(R.id.fragment_container, fragment);
-        transaction.commit();
     }
 
 }
--- app/src/main/java/com/formagrid/hellotest/HelloFragment.java
+++ app/src/main/java/com/formagrid/hellotest/HelloFragment.java
@@ -1,12 +1,27 @@
 package com.formagrid.hellotest;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class HelloFragment extends Fragment {
+
+    public static final String TRANSACTION_BACKSTACK_TAG = "hellofragment_backstack_tag";
+
+    public static void showFragment(Activity activity) {
+        FragmentManager manager = activity.getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.setCustomAnimations(R.animator.slide_in_up, R.animator.slide_out_down);
+        HelloFragment fragment = new HelloFragment();
+        transaction.add(R.id.fragment_container, fragment);
+        transaction.addToBackStack(TRANSACTION_BACKSTACK_TAG);
+        transaction.commit();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
