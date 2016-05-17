5ca6d7291b376c2dc756f51981087e16f7b7b167
Patricia Li <patricia.li@gmail.com>
2016-05-17 11:09:49 -0700

    notifyItemChanged test

--- app/src/main/java/com/formagrid/hellotest/HelloActivity.java
+++ app/src/main/java/com/formagrid/hellotest/HelloActivity.java
@@ -2,23 +2,30 @@ package com.formagrid.hellotest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class HelloActivity extends Activity {
+
+    private RecyclerView mRecyclerView;
+    private MyAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
 
-        TextView launchFragment = (TextView) findViewById(R.id.launch_fragment_text_view);
-        launchFragment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                HelloFragment.showFragment(HelloActivity.this);
-            }
-        });
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new MyAdapter();
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
@@ -34,6 +41,53 @@ public class HelloActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    private static class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView textView;
+
+            public ViewHolder(TextView view) {
+                super(view);
+
+                textView = view;
+            }
+
+        }
+
+        private String[] mItems;
+
+        public MyAdapter() {
+            mItems = Constants.STRINGS.clone();
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            TextView view = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.textView.setText(mItems[position]);
+
+            holder.textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = holder.getAdapterPosition();
+                    mItems[position] = "Trump";
+                    notifyItemChanged(position);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItems.length;
+        }
+
     }
 
 }
