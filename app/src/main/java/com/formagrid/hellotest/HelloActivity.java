512c0b4fe2c12fb2bc6453ea2263085139a6904e
Patricia Li <patricia.li@gmail.com>
2016-07-07 16:52:57 -0700

    IT DOESN'T FUCKING WORK

--- app/src/main/java/com/formagrid/hellotest/HelloActivity.java
+++ app/src/main/java/com/formagrid/hellotest/HelloActivity.java
@@ -1,49 +1,48 @@
 package com.formagrid.hellotest;
 
 import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class HelloActivity extends Activity {
 
-    private static final String FRAGMENT_TRANSACTION_ID = "first_transaction";
+    private RelativeLayout mRootLayout;
 
-    private FrameLayout mLeftContainer;
-    private FrameLayout mRightContainer;
-    private FrameLayout mFragmentContainer;
-
-    private boolean mIsFragmentOnRight;
+    private RecyclerView mRecyclerView;
+    private RecyclerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
 
-        mLeftContainer = (FrameLayout) findViewById(R.id.left_container);
-        mRightContainer = (FrameLayout) findViewById(R.id.right_container);
-        mFragmentContainer = (FrameLayout) findViewById(R.id.fragment_container);
+        mRootLayout = (RelativeLayout) findViewById(R.id.root_layout);
+        mRootLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("patricia", "mRootLayout click");
+            }
+        });
 
-        FragmentManager manager = getFragmentManager();
-        FragmentTransaction firstTransaction = manager.beginTransaction();
-        firstTransaction.add(R.id.fragment_container, new FirstFragment(), FRAGMENT_TRANSACTION_ID);
-        firstTransaction.commit();
-
-        mIsFragmentOnRight = true;
-    }
-
-    public void switchContainer(View view) {
-        if (mIsFragmentOnRight) {
-            mRightContainer.removeView(mFragmentContainer);
-            mLeftContainer.addView(mFragmentContainer);
-        } else {
-            mLeftContainer.removeView(mFragmentContainer);
-            mRightContainer.addView(mFragmentContainer);
-        }
-
-        mIsFragmentOnRight = !mIsFragmentOnRight;
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new RecyclerAdapter(Arrays.asList("hi", "this", "is", "some", "text"));
+        mRecyclerView.setAdapter(mAdapter);
+//        mRecyclerView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.d("patricia", "mRecyclerView click");
+//            }
+//        });
     }
 
     @Override
@@ -59,6 +58,43 @@ public class HelloActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Holder> {
+
+        public class Holder extends RecyclerView.ViewHolder {
+
+            protected TextView textView;
+
+            public Holder(TextView itemView) {
+                super(itemView);
+                this.textView = itemView;
+            }
+
+        }
+
+        private List<String> contents;
+
+        public RecyclerAdapter(List<String> contents) {
+            this.contents = contents;
+        }
+
+        @Override
+        public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+            TextView textView = new TextView(parent.getContext());
+            return new Holder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(Holder holder, int position) {
+            holder.textView.setText(contents.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return contents.size();
+        }
+
     }
 
 }
